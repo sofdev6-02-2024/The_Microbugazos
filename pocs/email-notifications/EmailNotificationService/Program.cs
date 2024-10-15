67b73d643768271f41a14e8628b916443c5f1b8b
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-10-15 15:43:19 -0400

    feat: finish example

---
+++ pocs/email-notifications/EmailNotificationService/Program.cs
@@ -0,0 +1,24 @@
+﻿using EmailNotificationService.Concretes;
+using DotNetEnv;
+
+namespace EmailNotificationService;
+
+public static class Program
+{
+    private static async Task InitExecution()
+    {
+        Env.Load();
+
+        var emailUsername = Environment.GetEnvironmentVariable("EMAIL_USERNAME");
+        var emailPassword = Environment.GetEnvironmentVariable("EMAIL_PASSWORD");
+        var emailReceiver = Environment.GetEnvironmentVariable("RECEIVER_USERNAME");
+        
+        var gmailService = new GmailService(emailUsername!, emailPassword!);
+        await gmailService.SendSimpleNotificationAsync(emailReceiver!);
+    }
+
+    public static void Main(string[] args)
+    {
+        InitExecution().Wait();
+    }
+}
