3c60a3343e4b63001edea4c4ecd49e79d91abe5d
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-10-27 13:35:59 -0400

    feat: adding dtos

---
+++ apps/merchant-api/InventoryService/src/InventoryService.Application/Dtos/PaginatedResponseDto.cs
@@ -0,0 +1,9 @@
+namespace InventoryService.Application.Dtos;
+
+public class PaginatedResponseDto<T>
+{
+    public List<T>? Items { get; set; }
+    public int TotalCount { get; set; }
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+}
