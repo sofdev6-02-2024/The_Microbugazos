cbc8c1868d51e3c9faded4a8cf4ec81eec58e692
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-10-30 16:40:36 -0400

    refactor: update base repository to override its methods

--- apps/merchant-api/InventoryService/src/InventoryService.Intraestructure/Repositories/Concretes/CategoryRepository.cs
+++ apps/merchant-api/InventoryService/src/InventoryService.Intraestructure/Repositories/Concretes/CategoryRepository.cs
@@ -1,7 +1,28 @@
 using InventoryService.Intraestructure.Data;
 using InventoryService.Intraestructure.Repositories.Bases;
 using InventoryService.Domain.Concretes;
+using Microsoft.EntityFrameworkCore;
 
 namespace InventoryService.Intraestructure.Repositories.Concretes;
 
-public class CategoryRepository(InventoryDbContext context) : BaseRepository<Category>(context);
+public class CategoryRepository(InventoryDbContext context) : BaseRepository<Category>(context)
+{
+    public override async Task<Category?> GetByIdAsync(Guid id)
+    {
+        return await DbSet
+            .Include(c => c.ParentCategory)          
+            .Include(c => c.SubCategories)         
+            .FirstOrDefaultAsync(c => c.Id == id);  
+    }
+
+    public override async Task<IEnumerable<Category>> GetAllAsync(int pageNumber, int pageSize)
+    {
+        return await DbSet
+            .Where(e => e.IsActive)
+            .Include(c => c.ParentCategory)       
+            .Include(c => c.SubCategories)          
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+    }
+}
