0016219991bc3236f546bde6228daeb1373ebf82
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-10-30 16:41:28 -0400

    feat: update commands and queries following observations

--- apps/merchant-api/InventoryService/src/InventoryService.Application/Dtos/Categories/CategoryDto.cs
+++ apps/merchant-api/InventoryService/src/InventoryService.Application/Dtos/Categories/CategoryDto.cs
@@ -4,7 +4,7 @@ using Domain.Concretes;
 
 public class CategoryDto
 {
-    public string? FatherCategoryName { get; set; }
     public Guid? Id { get; set; }
     public string? Name { get; set; }
+    public List<string>? SubCategories { get; set; } = new();
 }
