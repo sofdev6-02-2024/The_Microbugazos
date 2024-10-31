c2d67584f52b7f08d77ad3375eebce41fd9d8c27
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-10-31 13:21:14 -0400

    refactor: update repositories to not accept repetitive data

--- apps/merchant-api/InventoryService/src/InventoryService.Intraestructure/Repositories/Concretes/CategoryRepository.cs
+++ apps/merchant-api/InventoryService/src/InventoryService.Intraestructure/Repositories/Concretes/CategoryRepository.cs
@@ -7,6 +7,16 @@ namespace InventoryService.Intraestructure.Repositories.Concretes;
 
 public class CategoryRepository(InventoryDbContext context) : BaseRepository<Category>(context)
 {
+    public override async Task<Category> AddAsync(Category entity)
+    {
+        var existingCategory = await DbSet.FirstOrDefaultAsync(c => c.Name == entity.Name && c.IsActive);
+        if (existingCategory != null) return existingCategory; 
+        
+        await DbSet.AddAsync(entity);
+        await Context.SaveChangesAsync();
+        return entity;
+    }
+
     public override async Task<Category?> GetByIdAsync(Guid id)
     {
         return await DbSet
