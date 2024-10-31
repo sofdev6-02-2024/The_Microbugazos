780c70e590015295e207854be7eacbc2806c5269
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-10-31 08:34:16 -0400

    feat: adding getall without pagination

--- apps/merchant-api/InventoryService/src/InventoryService.Application/QueryCommands/Categories/Queries/Queries/GetAllCategoriesQuery.cs
+++ apps/merchant-api/InventoryService/src/InventoryService.Application/QueryCommands/Categories/Queries/Queries/GetAllCategoriesQuery.cs
@@ -4,8 +4,4 @@ using MediatR;
 
 namespace InventoryService.Application.QueryCommands.Categories.Queries.Queries;
 
-public class GetAllCategoriesQuery(int page, int pageSize) : IRequest<PaginatedResponseDto<CategoryDto>>
-{
-    public int Page { get; set; } = page;
-    public int PageSize { get; set; } = pageSize;
-}
+public class GetAllCategoriesQuery : IRequest<List<CategoryDto>>;
