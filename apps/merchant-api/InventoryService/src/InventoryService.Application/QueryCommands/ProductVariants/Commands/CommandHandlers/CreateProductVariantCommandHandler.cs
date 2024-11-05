ee429ecf31a5ada50065cc3ea1fa745fb175c5f6
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-11-05 11:35:44 -0400

    refactor: update commands for categories and productvariants

--- apps/merchant-api/InventoryService/src/InventoryService.Application/QueryCommands/ProductVariants/Commands/CommandHandlers/CreateProductVariantCommandHandler.cs
+++ apps/merchant-api/InventoryService/src/InventoryService.Application/QueryCommands/ProductVariants/Commands/CommandHandlers/CreateProductVariantCommandHandler.cs
@@ -1,5 +1,6 @@
 using InventoryService.Application.Dtos.ProductVariants;
 using InventoryService.Application.QueryCommands.ProductVariants.Commands.Commands;
+using InventoryService.Application.Services;
 using InventoryService.Domain.Concretes;
 using InventoryService.Intraestructure.Repositories.Interfaces;
 using MediatR;
@@ -8,9 +9,7 @@ namespace InventoryService.Application.QueryCommands.ProductVariants.Commands.Co
 
 public class CreateProductVariantCommandHandler(
     IRepository<Product> productRepository,
-    IRepository<Variant> variantRepository,
-    IRepository<Image> imageRepository,
-    IRepository<ProductVariant> productVariantRepository)
+    ProductVariantService productVariantService)
     : IRequestHandler<CreateProductVariantCommand, ProductVariantDto>
 {
     public async Task<ProductVariantDto> Handle(CreateProductVariantCommand request,
@@ -22,44 +21,8 @@ public class CreateProductVariantCommandHandler(
         if (product == null)
             throw new KeyNotFoundException($"The product's id {productVariantDto.ProductId} doesn't exist.");
 
-        Image? image = null;
-        if (productVariantDto.Image != null)
-            image = imageRepository.AddAsync(
-                    new Image
-                    {
-                        ProductId = productVariantDto.ProductId, Url = productVariantDto.Image!.Url,
-                        AltText = productVariantDto.Image.AltText
-                    })
-                .Result;
-
-
-        var productVariant = new ProductVariant
-        {
-            Image = image,
-            ProductId = productVariantDto.ProductId,
-            PriceAdjustment = productVariantDto.PriceAdjustment,
-            StockQuantity = productVariantDto.StockQuantity,
-            Attributes = new List<ProductAttribute>()
-        };
-
-        foreach (var attribute in productVariantDto.Attributes)
-        {
-            var variant = await variantRepository.AddAsync(new Variant { Name = attribute.Name });
-            var productAttribute = new ProductAttribute
-            {
-                ProductVariant = productVariant,
-                Variant = variant,
-                Value = attribute.Value
-            };
-            productVariant.Attributes.Add(productAttribute);
-            productAttributes.Add(new GetProductVariantAttributeDto
-            {
-                ProductVariantAttributeId = productAttribute.Id, Name = productAttribute.Variant.Name, Value = productAttribute.Value
-            });
-        }
-
-        var productVariantCreated = await productVariantRepository.AddAsync(productVariant);
-        return new ProductVariantDto { ProductVariantId = productVariantCreated.Id, 
+        var productVariant = await productVariantService.CreateProductVariant(productVariantDto, product.Id);
+        return new ProductVariantDto { ProductVariantId = productVariant.Id, 
             ProductId = product.Id, Attributes = productAttributes };
     }
 }
