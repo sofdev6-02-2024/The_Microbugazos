d69df6bf3c6cf91fea0781b94f7c022faecd5460
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-11-18 12:25:31 -0400

    feat: adding validators for products

---
+++ apps/merchant-api/InventoryService/src/InventoryService.Application/Validators/Products/ProductVariantValidator.cs
@@ -0,0 +1,37 @@
+using FluentValidation;
+using InventoryService.Application.Dtos.Products;
+using InventoryService.Application.Validators.ProductVariants;
+using InventoryService.Application.ValidatorSettings;
+using Microsoft.Extensions.Options;
+
+namespace InventoryService.Application.Validators.Products;
+
+public class ProductVariantValidator : AbstractValidator<ProductVariantForCreateDto>
+{
+    public ProductVariantValidator(IOptions<ValidationSettings> validationSettings)
+    {
+        var productVariantSettings = validationSettings.Value.ProductVariant;
+        
+        RuleFor(pv => pv.Image)
+            .SetValidator(new ProductVariantImageDtoValidator(validationSettings)) 
+            .When(x => x.Image != null);
+
+        RuleFor(x => x.PriceAdjustment)
+            .NotNull().WithMessage("Price Adjustment cannot be null.")
+            .GreaterThan(productVariantSettings.PriceAdjustmentMin)
+            .WithMessage($"The Price Adjustment must be greater than {productVariantSettings.PriceAdjustmentMin}.");
+
+        RuleFor(x => x.StockQuantity)
+            .NotNull().WithMessage("Stock Quantity cannot be null.")
+            .GreaterThan(productVariantSettings.StockQuantityMin)
+            .WithMessage($"The Stock Quantity must be greater than {productVariantSettings.StockQuantityMin}.");
+
+        RuleFor(x => x.Attributes)
+            .NotNull().WithMessage("Attributes is required")
+            .Must(o => o is { Count: >= 1 }).WithMessage("Product Variant must have at least one attribute.");  
+        
+        RuleForEach(x => x.Attributes)
+            .SetValidator(new ProductVariantAttributeDtoValidator(validationSettings))
+            .When(x => x.Attributes.Count != 0);
+    }
+}
