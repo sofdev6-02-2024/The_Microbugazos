a17cec78f3f1235749c01a1a8b97f60d919f2d7b
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-11-22 06:31:02 -0400

    feat: inyecting dependencies and add endpoint to process webhook event

--- apps/merchant-api/PaymentService/src/PaymentService.Api/Controllers/StripeController.cs
+++ apps/merchant-api/PaymentService/src/PaymentService.Api/Controllers/StripeController.cs
@@ -4,6 +4,7 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using PaymentService.Application.Dtos.CheckoutSessions;
 using PaymentService.Application.QueryCommands.StripeCheckoutSessions.Commands.CommandHandlers;
+using PaymentService.Application.QueryCommands.StripeWebHookRegister.Commands.Commands;
 
 namespace PaymentService.Api.Controllers;
 
@@ -11,10 +12,22 @@ namespace PaymentService.Api.Controllers;
 [Route("api/payment/[controller]")]
 public class StripeController(IMediator mediator) : ControllerBase
 {
-    [HttpPost("submit-cart")]
+    [HttpPost("checkout-session/submit-cart")]
     public ActionResult<BaseResponse> InitCheckoutSession([FromBody] CheckoutSessionRequestDto request)
     {
         var result = mediator.Send(new CreateCheckoutSessionCommand(request)).Result;
+        if (result is ErrorResponse errorResponse)
+            return StatusCode(errorResponse.StatusCode, errorResponse);
+
+        var successResponse = (SuccessResponse<string>)result;
+        return successResponse;
+    }
+    
+    [HttpPost("web-hook/register-web-hook-event")]
+    public async Task<ActionResult<BaseResponse>> ManageStripeWebHookDetection()
+    {
+        var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+        var result = await mediator.Send(new CreateEventRegisterWebHookCommand(json));
         if (result is ErrorResponse errorResponse)
             return StatusCode(errorResponse.StatusCode, errorResponse);
 
