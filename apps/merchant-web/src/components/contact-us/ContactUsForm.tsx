ef3fee6bf15a99ac27d6b15f8df86f36a26a28c8
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-11-28 01:50:24 -0400

    feat: adding components validations and page of contac us feature

---
+++ apps/merchant-web/src/components/contact-us/ContactUsForm.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import React from "react";
+import { FormikProps, useFormik } from "formik";
+import * as Yup from "yup";
+import { toast } from "sonner";
+import { EditableInput } from "@/components/atoms/inputs/EditableInput";
+import { ContactFormData } from "@/schemes/contact-us-form/ContactUsDto";
+import styles from "@/styles/contact-us/ContactUsForm.module.css"
+import {createContactUsMessage} from "@/request/ContactUsMessageRequest";
+
+const defaultContactData: ContactFormData = {
+  name: "",
+  email: "",
+  message: "",
+  reloadable: false,
+};
+
+const ContactFormSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(5, "Name should be at least 5 characters")
+    .max(100, "Name should not exceed 100 characters")
+    .required("Name is required"),
+  email: Yup.string()
+    .email("Invalid email format")
+    .required("Email is required"),
+  message: Yup.string()
+    .min(10, "Message should be at least 10 characters")
+    .max(500, "Message should not exceed 500 characters")
+    .required("Message is required"),
+});
+
+const ContactForm: React.FC = () => {
+  const formik: FormikProps<ContactFormData> = useFormik<ContactFormData>({
+    initialValues: defaultContactData,
+    validationSchema: ContactFormSchema,
+    onSubmit: async (values, { setSubmitting, resetForm }) => {
+      try {
+        const response = await createContactUsMessage(values);
+        console.log(response != null);
+        if (response != null) {
+          toast.success('Your request has been sent. It will be analyzed and you will receive a reply to the e-mail address you provided.');
+          resetForm();
+        } else {
+          toast.error('Error submitting the form. Please try again.');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+        toast.error('Error submitting the form. Please try again.');
+      }
+      finally {
+        setSubmitting(false);
+      }
+    },
+  });
+
+  const onBlur = (field: string) => {
+    formik.handleBlur(field);
+    formik.setFieldTouched(field, true);
+  };
+
+  return (
+    <div className={styles["contact-us"]}>
+      <form
+        onSubmit={formik.handleSubmit}
+      >
+        <EditableInput
+          type="text"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          label="Name"
+          placeholder="Enter your name"
+          name="name"
+          id="name"
+          error={formik.errors.name}
+          handleBlur={onBlur}
+          touched={formik.touched.name}
+          isEditable={true}
+        />
+
+        <EditableInput
+          type="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          label="Email"
+          placeholder="Enter your email"
+          name="email"
+          id="email"
+          error={formik.errors.email}
+          handleBlur={onBlur}
+          touched={formik.touched.email}
+          isEditable={true}
+        />
+
+        <EditableInput
+          type="text"
+          value={formik.values.message}
+          onChange={formik.handleChange}
+          label="Message"
+          placeholder="Enter your message"
+          name="message"
+          id="message"
+          error={formik.errors.message}
+          handleBlur={onBlur}
+          touched={formik.touched.message}
+          isEditable={true}
+          multiline={true}
+          rows={6}
+        />
+
+        <button
+          type="submit"
+          disabled={formik.isSubmitting || !formik.isValid}
+        >
+          {formik.isSubmitting ? 'Sending...' : 'Submit'}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
