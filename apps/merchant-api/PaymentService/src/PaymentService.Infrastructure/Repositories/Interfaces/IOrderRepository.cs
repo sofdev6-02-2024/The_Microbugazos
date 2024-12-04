134fb3fa8a3cc136be9e02d1db98ca2a69a4dcdf
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-12-03 23:54:53 -0400

    refactor: made changes into infraestructure and domain layer abour how repository manges order

--- apps/merchant-api/PaymentService/src/PaymentService.Infrastructure/Repositories/Interfaces/IOrderRepository.cs
+++ apps/merchant-api/PaymentService/src/PaymentService.Infrastructure/Repositories/Interfaces/IOrderRepository.cs
@@ -1,8 +1,10 @@
+using PaymentService.Commons.Params;
 using PaymentService.Domain.Entities.Concretes;
 
 namespace PaymentService.Infrastructure.Repositories.Interfaces;
 
 public interface IOrderRepository : IRepository<Order>
 {
-    public Task<IEnumerable<Order>> GetAllOrdersBySpecificUserAsync(Guid userId, int pageNumber, int pageSize);
+    public Task<IEnumerable<Order>> GetAllOrdersBySpecificUserAsync(Guid userId, OrderFilterParameters parameters, int pageNumber, int pageSize);
+    public Task<int> GetCountBySpecificUserAsync(Guid userId, OrderFilterParameters parameters);
 }
