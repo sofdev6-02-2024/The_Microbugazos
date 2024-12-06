464e5577d6c60f50b03793b27aa919083d22b1a6
daniel-espinoza-79 <daniel.espinoza.8812451@usalesiana.edu.bo>
2024-12-06 00:41:17 -0400

    refactor : adding sellers to list of notificator

--- apps/merchant-api/UserService/src/UserService.Application/Handlers/Users/RequestHandlers/Commands/NotifyLowStockUsersCommandHandler.cs
+++ apps/merchant-api/UserService/src/UserService.Application/Handlers/Users/RequestHandlers/Commands/NotifyLowStockUsersCommandHandler.cs
@@ -4,37 +4,66 @@ using MassTransit;
 using MediatR;
 using NotificationService.Domain.Dtos;
 using NotificationService.Domain.Dtos.Emails;
+using NotificationService.Domain.Dtos.OrderItems;
 using UserService.Application.Handlers.Users.Request.Commands;
 using UserService.Infrastructure.Repositories.Interfaces;
 
 namespace UserService.Application.Handlers.Users.RequestHandlers.Commands;
 
 
-public class NotifyLowStockUsersCommandHandler(IUserRepository userRepository, IResponseHandlingHelper responseHandlingHelper, IBus producer) : IRequestHandler<NotifyLowStockUsersCommand, BaseResponse>
+public class NotifyLowStockUsersCommandHandler(IUserRepository userRepository, IStoreRepository storeRepository, IResponseHandlingHelper responseHandlingHelper, IBus producer) : IRequestHandler<NotifyLowStockUsersCommand, BaseResponse>
 {
     public async Task<BaseResponse> Handle(NotifyLowStockUsersCommand request, CancellationToken cancellationToken)
     {
         foreach (var lowStockDetail in request.LowStockEmails)
         {
-            var users = await userRepository.GetByAsync((user) => user.Store != null && user.Store.Id == lowStockDetail.Key);
-
-            var ownerUser = users.First();
-            if (ownerUser == null)
+            Guid storeID = lowStockDetail.Key;
+            var store = await storeRepository.GetByIdAsync(storeID);
+            if (store == null)
             {
-                return responseHandlingHelper.BadRequest<bool>("The users with the follow store id " + lowStockDetail.Key + " was not found", null);
+                return responseHandlingHelper.BadRequest<bool>("The store with the follow id " + lowStockDetail.Key + " was not found", null);
             }
 
-            await producer.Publish(new LowStockEmail(
-                new Contact(
-                    ownerUser.Name ?? string.Empty,
-                    ownerUser.Email ?? string.Empty
-                ),
-                lowStockDetail.Value,
-                Environment.GetEnvironmentVariable("ADMIN_PANEL_URL") ?? string.Empty
-            ));
+            List<Guid> sellerIds = [.. store.SellerIds];
+
+
+            var users = await userRepository.GetByAsync((user) => user.Store != null && user.Store.Id == storeID);
+
+            if (users.Count() == 0)
+            {
+                return responseHandlingHelper.BadRequest<bool>("The users with the follow store id " + storeID + " were not found", null);
+            }
+            var usersList = users.ToList();
+
+            foreach (var sellerId in sellerIds)
+            {
+                var seller = await userRepository.GetByIdAsync(sellerId);
+                if (seller != null)
+                {
+                    usersList.Add(seller);
+                }
+            }
+
+            foreach (var user in usersList)
+            {
+                await SendEmail(user.Name, user.Email, lowStockDetail.Value);
+            }
         }
 
         return responseHandlingHelper.Ok("The users have been successfully notified.", true);
+    }
+
+
+    private async Task SendEmail(string? name, string? email, List<OrderItem> lowStockDetail)
+    {
+        await producer.Publish(new LowStockEmail(
+                new Contact(
+                    name ?? string.Empty,
+                    email ?? string.Empty
+                ),
+                lowStockDetail,
+                Environment.GetEnvironmentVariable("ADMIN_PANEL_URL") ?? string.Empty
+                ));
     }
 }
 
